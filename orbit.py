2b25b9c2aeeb983e4a44cb607edf8d4845eee939
Julius-1606 <crook011606@gmail.com>
2025-12-26 18:18:58 -0800

    Downgraded from gemini 2.0 to gemin 1.5

--- orbit.py
+++ orbit.py
@@ -12,7 +12,7 @@ CHAT_ID = "6882899041"
 
 # --- CONFIGURATION ---
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel('gemini-2.0-flash')
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 def load_config():
     # Get the directory where THIS script is located
@@ -90,3 +90,4 @@ if __name__ == "__main__":
         print("❌ Error: You forgot to paste your API keys in orbit.py!")
     else:
         asyncio.run(send_chaos())
+
